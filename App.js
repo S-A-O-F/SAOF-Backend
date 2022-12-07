08be61dc4934a28549223ad302ecfe4001dd6545
Javier Plaza Sisqués <javierplazasisques@gmail.com>
2022-12-07 20:01:10 +0100

    Add logger class

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 
+// Setup the logger
+const logger = require('./src/util/logger')
+
 const express = require('express')
 const app = express()
 const port = process.env.PORT
